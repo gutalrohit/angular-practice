2209787172f5c426ae67e48ef78af3b15925d398
Rohit Gutal <rohitgutal@rohits-mbp.home>
2020-12-30 19:33:51 +0100

    directives practice

--- src/app/components/server/server.component.ts
+++ src/app/components/server/server.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-@Component ({
-  selector: 'app-server',
+@Component({
+  selector: "app-server",
   // template: `<div class="server__font">Server is running</div>`,
-  templateUrl: './server.component.html',
+  templateUrl: "./server.component.html",
   // styles: [`
   //   .server__font {
   //     color: blue;
   //   }
   // `]
-  styleUrls: ['./server.component.scss']
+  styleUrls: ["./server.component.scss"]
 })
-
 export class ServerComponent {
-  serverNumber: string = '2';
-  anotherServer: string = 'Another server is also running';
-  enteredName: string = '';
+  serverNumber: string = "2";
+  anotherServer: string = "Another server is also running";
+  enteredName: string = "";
+  fontColor = "red";
+  showngif = false;
+  showServers = [1, 2, 3, 4, 5];
   clickHandler() {
-    alert('Click Button was clicked');
+    alert("Click Button was clicked");
   }
-
 }
